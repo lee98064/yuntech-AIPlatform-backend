5fa96925b5e24c39e4c31d5de8fc0269af198e46
Ryan Lee <lee98064@gmail.com>
2022-10-01 16:06:06 +0800

    Add upload and get studentImg function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 const { Student, Group } = require("../models");
 
 router.get("/user", async (req, res) => {
@@ -45,6 +46,75 @@ router.get("/user/group", async (req, res) => {
   });
 
   return res.json(group);
+});
+
+function getExtension(filename) {
+  var i = filename.lastIndexOf(".");
+  return i < 0 ? "" : filename.substr(i);
+}
+
+router.post("/user/upload", async (req, res) => {
+  const tokenInfo = req.tokenInfo;
+
+  const student = await Student.findOne({
+    where: {
+      id: tokenInfo.id,
+    },
+  });
+
+  try {
+    if (!req.files) {
+      return res.status(500).send({
+        status: false,
+        message: "沒有上傳檔案！！",
+      });
+    }
+
+    //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+    let image = req.files.image;
+
+    //Use the mv() method to place the file in the upload directory (i.e. "uploads")
+    let path = `./uploads/${student.studentID}${getExtension(image.name)}`;
+    image.mv(path);
+
+    if (student.studentImg != null) {
+      fs.unlinkSync(student.studentImg);
+    }
+
+    student.studentImg = path;
+    student.isVerify = false;
+
+    await student.save();
+
+    //send response
+    res.send({
+      status: true,
+      message: "上傳成功！",
+      data: {
+        name: image.name,
+        mimetype: image.mimetype,
+        size: image.size,
+      },
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.get("/user/studentImg", async (req, res) => {
+  const tokenInfo = req.tokenInfo;
+
+  const student = await Student.findOne({
+    where: {
+      id: tokenInfo.id,
+    },
+  });
+
+  if (student.studentImg == null) {
+    return res.sendStatus(404);
+  }
+
+  res.download(student.studentImg); // Set disposition and send it.
 });
 
 module.exports = router;
