bc22c563c91a3f7d7dd09caa792dd9d8da0269a0
Ryan Lee <lee98064@gmail.com>
2022-10-01 16:06:43 +0800

    Install upload package

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 const authController = require("./controllers/authController.js");
 const postController = require("./controllers/postController.js");
 const userController = require("./controllers/userController.js");
@@ -16,6 +17,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({ createParentPath: true }));
 
 app.get("/", (req, res) => {
   res.send("What are you doing?");
