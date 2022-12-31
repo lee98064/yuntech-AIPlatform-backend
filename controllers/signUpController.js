954a1a53b73751d583a0eb35652c0fa6cf5a75df
Ryan Lee <lee98064@gmail.com>
2022-12-31 19:32:03 +0800

    Add log method to mongo

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -6,6 +6,7 @@ const notSignUp = require("../middlewares/notSignUp");
 const Mailer = require("../services/mailer");
 const RandomString = require("../services/randomString");
 const signUpTime = require("../middlewares/signUpTime");
+const requestIp = require("request-ip");
 
 router.post("/signUp/createGroup", signUpTime, async (req, res) => {
   const tokenInfo = req.tokenInfo;
@@ -69,6 +70,7 @@ router.post("/signUp/createGroup", signUpTime, async (req, res) => {
     student: student.studentID,
     studentName: student.name,
     datetime: new Date(),
+    ip: requestIp.getClientIp(req),
   });
 
   return res.json({
@@ -127,6 +129,16 @@ router.post("/signUp/joinGroup", signUpTime, async (req, res) => {
 
   group.isVerify = false;
   await group.save();
+
+  mgdb.collection("student").insertOne({
+    method: "joinGroup",
+    groupId: group.id,
+    group: group.name,
+    student: student.studentID,
+    studentName: student.name,
+    datetime: new Date(),
+    ip: requestIp.getClientIp(req),
+  });
 
   return res.json({
     status: true,
